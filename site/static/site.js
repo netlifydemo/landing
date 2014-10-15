cfd0202524e4ed095381a9f19635c9a9ea996f27
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-10-14 22:07:18 -0700

    Make logo a link

--- site/static/site.js
+++ site/static/site.js
@@ -1,9 +1,17 @@
 angular.module('netlifyApp', []).controller('howCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 
 
-}]).controller('modalCtrl', ['$scope', function($scope) {
-  $scope.signup = function() {
-    $scope.modal = true;
+}]).controller('modalCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+  $scope.signup = function($event) {
+    $timeout(function() {
+      $scope.modal = true;
+    }, 1);
+  }
+
+  $scope.closeModal = function($event) {
+    console.log($event)
+    $scope.modal = false;
+
   }
 
 }]);
