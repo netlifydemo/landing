88d3c8ea91e7df2f98e6fbdc35aec1e5992d255c
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-10-14 20:19:27 -0700

    Make the signup button work

--- site/static/site.js
+++ site/static/site.js
@@ -1,4 +1,9 @@
 angular.module('netlifyApp', []).controller('howCtrl', ['$scope', '$timeout', function($scope, $timeout) {
-  
+
+
+}]).controller('modalCtrl', ['$scope', function($scope) {
+  $scope.signup = function() {
+    $scope.modal = true;
+  }
 
 }]);
